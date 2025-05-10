7b960ab95d9f9e08d7bafbb541ee19b8800e965a
amanjha79 <amanjha9808@gmail.com>
2025-05-10 21:02:33 +0530

    feat(weather-app): add weather app component and integrate react-icons
    Added a new Weather component to display current weather information. Integrated react-icons for the search functionality. Updated package.json and package-lock.json to include react-icons dependency. Modified App.jsx to render the Weather component instead of the Quotes component.

--- basic projects/src/App.jsx
+++ basic projects/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import Weather from './projects/weather App/Weather'
 // import Todo from './projects/Todo/Todo'
-import Quotes from './projects/Quotes genertor/Quotes'
+// import Quotes from './projects/Quotes genertor/Quotes'
 // import Clock from './projects/digital clock/Clock'
 // import Calculator from './projects/Basic calculator/Calculator'
 // import Colorpicker from './projects/color picker/Colorpicker'
@@ -8,13 +9,14 @@ import Quotes from './projects/Quotes genertor/Quotes'
 
 const App = () => {
   return (
-    <section className='w-full p-10 bg-gray-900 text-white flex items-center justify-center flex-col gap-10'>
+    <section className='w-full min-h-screen p-10 bg-gray-900 text-white flex items-center justify-center flex-col gap-10'>
       {/* <Counter /> */}
       {/* <Colorpicker /> */}
       {/* <Clock /> */}
       {/* <Calculator /> */}
       {/* <Todo /> */}
-      <Quotes />
+      {/* <Quotes /> */}
+      <Weather />
     </section>
   )
 }
