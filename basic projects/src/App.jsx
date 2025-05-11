cdba9847fbeb9af6d74d1103427da2b214df8758
amanjha79 <amanjha9808@gmail.com>
2025-05-11 20:53:45 +0530

    refactor: uncomment and enable previously commented components in App.jsx
    This commit uncomments and enables several components in App.jsx that were previously commented out. This change allows all the project components to be rendered and tested in the application, improving the overall functionality and user experience.

--- basic projects/src/App.jsx
+++ basic projects/src/App.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
-// import Images from './projects/Gallery/Images'
-// import Quiz from './projects/Quiz App/Quiz'
+import Images from './projects/Gallery/Images'
+import Quiz from './projects/Quiz App/Quiz'
 import FormValidation from './projects/Form Validator/Form'
-// import Weather from './projects/weather App/Weather'
-// import Todo from './projects/Todo/Todo'
-// import Quotes from './projects/Quotes genertor/Quotes'
-// import Clock from './projects/digital clock/Clock'
-// import Calculator from './projects/Basic calculator/Calculator'
-// import Colorpicker from './projects/color picker/Colorpicker'
-// import Counter from './projects/counter/Counter'
+import Weather from './projects/weather App/Weather'
+import Todo from './projects/Todo/Todo'
+import Quotes from './projects/Quotes genertor/Quotes'
+import Clock from './projects/digital clock/Clock'
+import Calculator from './projects/Basic calculator/Calculator'
+import Colorpicker from './projects/color picker/Colorpicker'
+import Counter from './projects/counter/Counter'
 
 const App = () => {
   return (
     <section className='w-full min-h-screen p-10 bg-gray-900 text-white flex items-center justify-center flex-col gap-10'>
-      {/* <Counter />
+      <Counter />
       <Colorpicker />
       <Clock />
       <Calculator />
       <Todo />
-      <Quotes /> */}
-      {/* <Weather /> */}
-      {/* <Images /> */}
-      {/* <Quiz /> */}
+      <Quotes />
+      <Weather />
+      <Images />
+      <Quiz />
       <FormValidation/>
     </section>
   )
